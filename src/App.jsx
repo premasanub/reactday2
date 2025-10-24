d632fb867e01d205f5ebb945e86b207a8560cb22
premasanub <premakumaran15@gmail.com>
2025-10-24 19:06:40 +0530

    react day2 copy landing page completed

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Home from './Components/Home';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+
+const App = () => {
+  return (
+    <div>
+     <Header />
+      <Home />
+      <Footer />
+
+      
+    </div>
+  );
+};
+
+export default App;
